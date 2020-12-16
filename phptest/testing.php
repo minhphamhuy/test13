d604bd65ba09512d70033b90ba849b034e6f863f
mechanizespider <57589523+mechanizespider@users.noreply.github.com>
2020-12-17 00:43:12 +0700

    test 27

--- phptest/testing.php
+++ phptest/testing.php
@@ -1,2 +1,18 @@
 <?php
-echo "this is test:";
+namespace Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class tooltest extends TestCase
+{
+    public function testingtools()
+    {
+        $testemail = 'email123';
+        $result1 = false;
+        $result2 = true;
+        include '../tools.php';
+        $result= is_valid_email($testemail);
+        $this->assertEquals($result1, $result);
+    }
+}
